c0f153fe33de5cfadeb2e973bd80e93e41d325b7
GlebT98 <43340447+GlebT98@users.noreply.github.com>
2018-12-25 17:07:28 +0300

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    abstract class Figure: IPrint
+    {
+        public abstract double area();
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rect = new Rectangle(4, 7);
+            var square = new Square(8);
+            var circle = new Circle(10);
+
+            var figures = new Figure[] { rect, square, circle };
+
+            foreach (var figure in figures)
+            {
+                Console.WriteLine(figure.area());
+            }
+
+            var prints = new IPrint[] { rect, square, circle };
+
+            foreach (var print in prints)
+            {
+                print.Print();
+            }
+        }
+    }
+}
